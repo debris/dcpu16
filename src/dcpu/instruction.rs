ad8c200143db16a48a19deaad2c4297789a674a9
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-07-10 02:01:29 +0200

    special opcodes && cpu process functions stub

--- src/dcpu/instruction.rs
+++ src/dcpu/instruction.rs
@@ -1,6 +1,7 @@
 
-// check if nth bit is written and rewrite it to result
-fn getBitsInRange(word: u16, start: u8, length: u8) -> u16 {
+/// check if nth bit is written and rewrite it to result
+/// start is value in range 0 to 15
+fn get_bits_in_range(word: u16, start: u8, length: u8) -> u16 {
     let mut result: u16 = 0;
     for n in start..(start+length) {
         result |= (((word >> n) & 1) << (n - start));
@@ -8,20 +9,42 @@ fn getBitsInRange(word: u16, start: u8, length: u8) -> u16 {
     result
 }
 
-struct Instruction(u16);
+pub struct Instruction(pub u16);
 
 impl Instruction {
-    fn opcode(&self) -> u8 {
-        getBitsInRange(self.0, 0, 5) as u8
+    pub fn is_special(&self) -> bool {
+        self.opcode() == 0u8
     }
 
-    fn a(&self) -> u8 {
-        getBitsInRange(self.0, 10, 6) as u8
+    pub fn opcode(&self) -> u8 {
+        get_bits_in_range(self.0, 0, 5) as u8
     }
 
-    fn b(&self) -> u8 {
-        getBitsInRange(self.0, 5, 5) as u8
+    pub fn special_opcode(&self) -> u8 {
+        get_bits_in_range(self.0, 5, 5) as u8
     }
+
+    pub fn a(&self) -> u8 {
+        get_bits_in_range(self.0, 10, 6) as u8
+    }
+
+    pub fn b(&self) -> u8 {
+        get_bits_in_range(self.0, 5, 5) as u8
+    }
+}
+
+#[test]
+fn test_is_special() {
+    let i = Instruction(0b000011_00001_00000);
+    let i2 = Instruction(0b000011_00001_00001);
+    assert_eq!(true, i.is_special());
+    assert_eq!(false, i2.is_special());
+}
+
+#[test]
+fn special_opcode() {
+    let i = Instruction(0b000011_00001_00000);
+    assert_eq!(0b1u8, i.special_opcode());
 }
 
 #[test]
--- src/dcpu/mod.rs
+++ src/dcpu/mod.rs
@@ -1,20 +1,48 @@
 mod memory;
-mod Instruction;
+mod instruction;
+
+use self::memory::Memory as Memory;
+use self::instruction::Instruction as Instruction;
 
 pub struct Dcpu {
-    memory: memory::Memory
+    memory: Memory,
+    cyc: u16,   // cycle
+    pc: u16,    // program_counter
+    sp: u16,    // stack_pointer
+    ea: u16,    // extra_access
+    ia: u16     // interupt address
 }
 
 impl Default for Dcpu {
     fn default() -> Dcpu {
         Dcpu {
-            memory: Default::default()
+            memory: Default::default(),
+            cyc: 0,
+            pc: 0,
+            sp: 0,
+            ea: 0,
+            ia: 0
         }
     }
 }
 
 impl Dcpu {
-    fn process(&self, word: u16) {
+    fn process(&mut self, word: u16) {
+        let i = Instruction(word);
+        if i.is_special() {
+            self.process_special_instruction(&i);
+        } else {
+            self.process_instruction(&i);
+        }
     }
+
+    fn process_instruction(&mut self, i: &Instruction) {
+
+    }
+
+    fn process_special_instruction(&mut self, i: &Instruction) {
+    }
+
+
 }
 
