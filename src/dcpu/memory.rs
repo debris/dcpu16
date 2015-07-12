97a6c2110368e9bb1543f70613a6c232920f17a9
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-07-12 09:39:13 +0200

    reading words from memory

--- src/dcpu/memory.rs
+++ src/dcpu/memory.rs
@@ -1,10 +1,32 @@
 pub struct Memory {
-    memory: [u16; 10000]
+    memory: [u16; 10000],
+    loaded: usize             // how many words are loaded at the beginning of the memory
 }
 
 impl Default for Memory {
-    fn default() -> Memory { 
-        Memory { memory: [0; 10000] }
+    fn default() -> Memory {
+        Memory { 
+            memory: [0; 10000],
+            loaded: 0
+        }
+    }
+}
+
+impl Memory {
+    pub fn load(&mut self, words: &[u16]) {
+        let len = words.len();
+        for n in self.loaded..(self.loaded+len) {
+            self.memory[n] = words[n - self.loaded]; 
+        }
+        self.loaded += len;
+    }
+
+    pub fn get(&self, pos: usize) -> u16 {
+        self.memory[pos]
+    }
+
+    pub fn is_readable(&self, pos: usize) -> bool {
+        pos < self.loaded
     }
 }
 
--- src/dcpu/mod.rs
+++ src/dcpu/mod.rs
@@ -42,29 +42,42 @@ impl Default for Dcpu {
 }
 
 impl Dcpu {
-    fn process(&mut self, word: u16) {
-        let i = Instruction(word);
+    fn load(&mut self, words: &[u16]) {
+        self.memory.load(words);
+    }
+
+    fn read_word(&mut self) -> u16 {
+        let res = self.memory.get(self.pc as usize);
         self.pc += 1;
-        match i.opcode() {
-            Opcode::SET=> {
-                let a = self.get_value(i.a());
-                self.set_value(i.b(), a);
-            },
-            Opcode::ADD => {
-                let a = self.get_value(i.a());
-                let b = self.get_value(i.b());
-                self.set_value(i.b(), a + b);
-            },
-            _ => () 
+        res
+    }
+
+    fn process(&mut self) {
+        while self.memory.is_readable(self.pc as usize) {
+            let word = self.read_word();
+            let i = Instruction(word);
+            match i.opcode() {
+                Opcode::SET=> {
+                    let a = self.get_value(i.a());
+                    self.set_value(i.b(), a);
+                },
+                Opcode::ADD => {
+                    let a = self.get_value(i.a());
+                    let b = self.get_value(i.b());
+                    self.set_value(i.b(), a + b);
+                },
+                _ => () 
+            }
         }
     }
 
-    fn get_value(&self, addr: u8) -> Value {
+    fn get_value(&mut self, addr: u8) -> Value {
         match addr {
             n @ 0x0 ... 0x7 => self.registers[n as usize],
             0x1b => self.sp,
             0x1c => self.pc,
             0x1d => self.ex,
+            0x1f => self.read_word() as Value,
             n @ 0x20 ... 0x3f => (n - 0x21) as Value,
             _ => 0
         }
@@ -81,17 +94,25 @@ impl Dcpu {
 #[test]
 fn test_set() {
     let mut cpu: Dcpu = Default::default();
-    cpu.process(0xfc01); // SET A, 30
+    cpu.load(&[
+             0xfc01,        // SET A, 30
+             0x7c21, 0x001f // SET B, 31
+    ]);
+    cpu.process();
     assert_eq!(cpu.registers[0], 30);
-    assert_eq!(cpu.pc, 1);
+    assert_eq!(cpu.registers[1], 31);
+    assert_eq!(cpu.pc, 3);
 }
 
 #[test]
 fn test_add() {
     let mut cpu: Dcpu = Default::default();
-    cpu.process(0xfc01); // SET A, 30
-    cpu.process(0x8821); // SET B, 1
-    cpu.process(0x0022); // ADD B, A
+    cpu.load(&[
+             0xfc01,    // SET A, 30
+             0x8821,    // SET B, 1
+             0x0022     // ADD B, A
+    ]);
+    cpu.process(); 
     assert_eq!(cpu.registers[0], 30);
     assert_eq!(cpu.registers[1], 31);
     assert_eq!(cpu.pc, 3);
